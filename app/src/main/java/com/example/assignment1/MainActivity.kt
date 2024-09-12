0b7e8161a56ff941e4592c4fff03e54e605a175a
RC_Student_lab <st10463105@rcconnect.edu.za>
2024-09-12 11:32:26 +0000

    First Commit

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -43,16 +43,16 @@ class MainActivity : AppCompatActivity() {
 // Function to match age to a famous person
 private fun matchAgeToFamousPerson(age: Int): String {
     return when (age) {
-        23 -> "Solomon Mahlangu"
-        31 -> "Steve Biko"
-        43 -> "Victoria Mxenge"
-        51 -> "Chris Hani"
-        57 -> "Ruth First"
-        69 -> "Joe Slovo"
-        76 -> "Oliver Tambo"
-        82 -> "Winnie Madikizela"
-        90 -> "Desmond Tutu"
-        95 -> "Nelson Mandela"
+        23 -> "Solomon Mahlangu, Anti-apartheid freedom fighter"
+        31 -> "Steve Biko, He was apartheid activist and leader of the Black Consciousness Movement"
+        43 -> "Victoria Mxenge, apartheid activist and lawyer, assassinated at age 43"
+        51 -> "Chris Hani, He leader of the South African Communist Party"
+        57 -> "Ruth First Anti-apartheid activist and scholar"
+        69 -> "Joe Slovo, apartheid activist and leader of the South African Communist Party"
+        76 -> "Oliver Tambo Anti-apartheid politician and a central figure in the African ANC"
+        82 -> "Winnie Madikizela, Anti-apartheid activist and politician"
+        90 -> "Desmond Tutu, Archbishop and social rights activist"
+        95 -> "Nelson Mandela, Anti-apartheid revolutionary and former President of South Africa"
         else -> "The age is an invalid format."
     }
 }
